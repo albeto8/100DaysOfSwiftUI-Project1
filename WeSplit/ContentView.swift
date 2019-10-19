519268cc24a65a88cb186ba0271c30c4e2856c35
Mario Alberto Barragán Espinosa <>
2019-10-19 12:01:53 -0500

    change the total amount text view to red if the user selects a 0% tip.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -68,7 +68,8 @@ struct ContentView: View {
         }
 
         Section(header: Text("Total amount")) {
-          Text("$\(totalAmount, specifier: "%.2f")")
+            Text("$\(totalAmount, specifier: "%.2f")")
+                .foregroundColor(tipPercentage == 4 ? Color.red : .black)
         }
       }
       .navigationBarTitle("WeSplit")
